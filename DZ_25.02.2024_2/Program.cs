3570c987540de359be8f5581e546afd4b5ceb022
Андрей <dusha22075@gmail.com>
2024-02-17 15:12:20 +0300

    Добавьте файлы проекта.

---
+++ DZ_25.02.2024_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры два числа. Первое число — это значение,
+// второе число процент, который необходимо посчитать. Например, мы ввели с клавиатуры 90 и 10.
+// Требуется вывести на экран 10 процентов от 90. Результат: 9.
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вычислим процент от числа.");
+        Console.WriteLine("Введите первое число:");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число (процент):");
+        double percent = Convert.ToDouble(Console.ReadLine());
+
+        double result = (number * percent) / 100;
+        Console.WriteLine("Результат: " + result);
+    }
+}
